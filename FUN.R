35b506b42b05bb57682d7353b21307b300764c96
sinawebgard <webgard@gmail.com>
2020-10-17 19:24:23 +0100

    updating the delay in iterations

--- FUN.R
+++ FUN.R
@@ -2,12 +2,11 @@
 get_queries <- function (keywords, parameters = para) { 
         
         trends <- gtrends(keywords, 
-                          gprop = "web",
                           geo = para$geo,
                           time = para$time,
                           tz= para$tz) 
-        Sys.sleep(2)
-        results <- trends$related_queries
+        Sys.sleep(sample(0:2, 1))
+        trends %>% .$related_queries
 }
 
 
@@ -16,12 +15,11 @@ check_trends <- function (keywords, parameters = para) {
 
         
         trends <- gtrends(keywords, 
-                          gprop = "web",
                           geo = para$geo,
                           time = para$time,
                           tz = para$tz,
                           onlyInterest = TRUE ) 
         trends$interest_over_time$hits[trends$interest_over_time$hits == "<1"] <- 0.5
         trends$interest_over_time$hits <- as.numeric(trends$interest_over_time$hits)
-        results <- trends$interest_over_time
+        trends %>% .$interest_over_time
 }
--- gtrends.R
+++ gtrends.R
@@ -13,15 +13,15 @@ if(exists("keywords")){
 if(file.exists("Data/unwanted.csv")) 
         unwanted.queries <- readLines("Data/unwanted.csv")
 
-# Setting the parameters
+########################### Setting the parameters#########
 para <- list(geo =  "IR", 
-             time = "now 1-H",
+             time = "now 4-H",
              tz = -60)
 
-index <- "آهنگ"
-        scale <- as.character()
-scale[1] <- "rising"
-scale[2] <- "top"
+index <- "سایت"         # up to 4 keywords as  benchmarks for trends
+scale <- as.character()         #selecting 'rising' and/or 'top' trends
+        scale[1] <- "rising"
+        scale[2] <- "top"
 
 ################# Getting Queries ########################
 ### getting related queries for all the terms 
@@ -30,10 +30,10 @@ scale[2] <- "top"
 
 readline("Press any key to get queries related to your keywords")
 
-        
+keyterms <- split(keyterms, 1: ceiling(length(keyterms) / 5))        
         queries.df <- map_dfr(.x = keyterms, .f = get_queries )
         
-        queries.df %>% filter(related_queries %in% scale) %>% pull(value) %>% 
+        queries.df %>% filter(related_queries %in% scale) %>% .$value %>% 
                 unique() %>% setdiff(unwanted.queries) -> queries
         
         
@@ -41,15 +41,21 @@ readline("Press any key to get queries related to your keywords")
 ############ Get relative popularity for calculated queries
 
 readline("Press any key to continue -- it may take few minutes")
+
+group_length <- 5- length(index)
+queries <- split(queries, 1: ceiling(length(queries) / group_length))
         
         trending_over_time <- vector()
                 for (i in 1:length(queries)) {
-                        trending_over_time <- c(index, queries[i]) %>% 
+                        trending_over_time <- c(index, queries[[i]]) %>% 
                                 check_trends() %>% 
                         rbind(trending_over_time)
-                        Sys.sleep(2)
-                if(round(i/50) == i/50) readline("Press any key to continue")
-                }
+####### slowing down the iteration 
+        sleep_time <- ifelse(i %% 50 == 0, 
+                             sample(10:15, 1), 
+                             sample(0:2, 1))
+        Sys.sleep(sleep_time)
+}
 trending_over_time <- filter(trending_over_time, !keyword %in% index)
 trending_over_time %>% group_by(keyword) %>% 
                         summarise(average = mean(hits, na.rm = TRUE), 
